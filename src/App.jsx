876af42298af059d50858744852e9cd3ce246f60
Amogh Shetty <amoggo2004@gmail.com>
2025-01-04 17:32:09 +0300

    Add game status section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-function App() {
+import React from 'react'
 
+function App() {
+    return (
+        <main>
+            <header>
+                <h1>Hangman</h1>
+                <p>Guess the word within 7 attempts to win!</p>
+            </header>
+            <section className="status">
+                <h1>You win!</h1>
+                <p>Well done! 🎉</p>
+            </section>
+        </main>
+    )
 }
 
 export default App
